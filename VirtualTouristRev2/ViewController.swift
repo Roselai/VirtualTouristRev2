05c3fe053b452bea1f647306c0300b9265e2c7b5
Shukti Shaikh <Shooktee@macbook-air.fios-router.home>
2017-09-27 14:14:18 -0400

    Initial Commit

---
+++ VirtualTouristRev2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTouristRev2
+//
+//  Created by Shukti Shaikh on 9/27/17.
+//  Copyright © 2017 Shukti Shaikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
